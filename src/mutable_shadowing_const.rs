155d9ce5bd9684c4e8864eab22923435cee48ebc
Leopold Johannes Meinel <leo@meinel.dev>
2022-07-28 17:08:29 +0200

    Update

--- src/mutable_shadowing_const.rs
+++ src/mutable_shadowing_const.rs
@@ -13,7 +13,7 @@
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program. If not, see https://github.com/TamrielNetwork/project/blob/main/LICENSE
+ * along with this program. If not, see https://github.com/LeoMeinel/project/blob/main/LICENSE
  */
 
 pub(crate) fn mutable_shadowing_const() {
